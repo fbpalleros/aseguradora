b2eff24d1358ff3bd24aea7d63e8f0fb3f43fd51
Facundo Palleros <facupls10@gmail.com>
2024-05-23 19:01:40 -0300

    customer repositoy test

--- src/test/java/org/aseguradora/repositories/CustomerRepositoryTest.java
+++ src/test/java/org/aseguradora/repositories/CustomerRepositoryTest.java
@@ -1,55 +1,46 @@
 package org.aseguradora.repositories;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 
 import java.util.List;
 
-import org.aseguradora.controllers.CotizacionController;
+import org.aseguradora.config.HibernateConfig;
 import org.aseguradora.entity.Customer;
-import org.aseguradora.entity.Policy;
-import org.aseguradora.services.CarService;
-import org.aseguradora.services.CustomerService;
-import org.aseguradora.services.InsuranceService;
-import org.aseguradora.services.PolicyService;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.aseguradora.repositories.impl.CustomerRepositoryImpl;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {HibernateConfig.class, CustomerRepositoryImpl.class})
 class CustomerRepositoryTest {
 
-	static private CustomerRepository customerRepostory;
-	
+	@Autowired
+	private CustomerRepository customerRepository;
+
+
 	private Long ID_TEST = 1L;
-	
-	@BeforeAll
-	public static void init() {
-        customerRepostory = mock(CustomerRepository.class);
-    }
-	
+
+
 	@Test
 	public void create() {
-		 Customer customer = new Customer(ID_TEST, "Test Name", "test2@gmail.com");
-		 customerRepostory.save(customer);
-		 assertNotNull(customer);
-	 }
-	
-	 @Test
-	 public void findByid() {
-		 Customer customer = customerRepostory.findOne(ID_TEST);
-		 assertNotNull(customer);
-	 }
-	
-	
-	 @Test
-	 public void findAll() {
-		 List<Customer> customer = customerRepostory.findAll();
-		 assertNotNull(customer);
-	 }
+		Customer customer = new Customer(ID_TEST, "Test Name", "test2@gmail.com");
+		customerRepository.save(customer);
+		assertNotNull(customer);
+	}
+
+	@Test
+	public void findByid() {
+		Customer customer = customerRepository.findOne(ID_TEST);
+		assertNotNull(customer);
+	}
+
+
+	@Test
+	public void findAll() {
+		List<Customer> customer = customerRepository.findAll();
+		assertNotNull(customer);
+	}
 }
