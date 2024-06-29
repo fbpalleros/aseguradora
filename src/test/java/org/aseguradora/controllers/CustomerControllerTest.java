998c556decad135dc8abebcddb85ec77952a91df
Facundo Palleros <facupls10@gmail.com>
2024-06-29 20:43:21 -0300

    modificando CustomerController y CustomerControllerTest

--- src/test/java/org/aseguradora/controllers/CustomerControllerTest.java
+++ src/test/java/org/aseguradora/controllers/CustomerControllerTest.java
@@ -1,8 +1,10 @@
 package org.aseguradora.controllers;
 
 import org.aseguradora.entity.Customer;
+import org.aseguradora.entity.Payment;
 import org.aseguradora.entity.Policy;
 import org.aseguradora.services.CustomerService;
+import org.aseguradora.services.PaymentService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,7 +14,6 @@ import javax.servlet.http.HttpSession;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -23,6 +24,7 @@ public class CustomerControllerTest {
     private CustomerController customerController;
 
     private CustomerService customerService;
+    private PaymentService paymentService;
 
     private HttpSession session;
     private HttpServletRequest request;
@@ -32,7 +34,8 @@ public class CustomerControllerTest {
         this.session = mock(HttpSession.class);
         this.request = mock(HttpServletRequest.class);
         this.customerService = mock(CustomerService.class);
-        this.customerController = new CustomerController(customerService);
+        this.paymentService = mock(PaymentService.class);
+        this.customerController = new CustomerController(customerService, paymentService);
     }
 
     @Test
@@ -145,22 +148,59 @@ public class CustomerControllerTest {
     }
 
     @Test
-    public void queRetorneUnaListaDePolizasPagadas(){
+    public void queAlIntentarPagarUnaPolizaRetorneLaVentanaDePago(){
         Customer customer = new Customer();
         customer.setId(1L);
-        Policy p1 = new Policy(1L, customer, 1000.00);
-        Policy p2 = new Policy(2L, customer, 1000.00);
-        List<Policy> policies = new ArrayList<>();
-        policies.add(p1);
-        policies.add(p2);
+
+        Policy policy = new Policy(1L, customer, 2000.00);
+        Payment payment = new Payment();
+        payment.setPolicy(policy);
+        payment.setCustomer(customer);
+        payment.setAmount(policy.getCoverage());
 
         when(request.getSession()).thenReturn(session);
         when(session.getAttribute("customer")).thenReturn(customer);
-        when(this.customerService.findPaidPoliciesByCustomerId(customer.getId())).thenReturn(policies);
+        when(this.customerService.findPolicyByIdCustomer(1L, 1L)).thenReturn(policy);
+
+        ModelAndView mav = this.customerController.pagar(1L, request);
+
+        assertThat(mav.getViewName(), equalToIgnoringCase("pago"));
+    }
+
+    @Test
+    public void queAlEnviarElPagoSeGuardeElMismoYSeActualiceLaPolizaEnCero(){
+        Customer customer = new Customer();
+        customer.setId(1L);
+        Policy policy = new Policy(1L, customer, 2000.00);
+        Payment payment = new Payment();
+        payment.setPolicy(policy);
+        payment.setCustomer(customer);
+        payment.setAmount(policy.getCoverage());
+
+        when(request.getSession()).thenReturn(session);
+        when(session.getAttribute("customer")).thenReturn(customer);
+        when(this.customerService.findPolicyByIdCustomer(1L, 1L)).thenReturn(policy);
+        ModelAndView mav = this.customerController.pagarConTarjeta(payment, request);
+
+        assertThat(mav.getViewName(), equalToIgnoringCase("redirect:/mis_pagos"));
+        verify(this.paymentService, times(1)).save(payment);
+        verify(this.customerService, times(1)).pay(policy);
+    }
+
+    @Test
+    public void queSeRetorneLaVentanaMisPagos(){
+        Customer customer = new Customer();
+        customer.setId(1L);
+
+        List<Payment> paidPolicies = new ArrayList<>();
+
+        when(request.getSession()).thenReturn(session);
+        when(session.getAttribute("customer")).thenReturn(customer);
+        when(this.paymentService.findByCustomerId(1L)).thenReturn(paidPolicies);
         ModelAndView mav = this.customerController.mostrarPagos(request);
 
         assertThat(mav.getViewName(), equalToIgnoringCase("mis_pagos"));
-        assertThat(mav.getModel().get("policies"), equalToObject(policies));
+        assertThat(mav.getModel().get("policies"), equalToObject(paidPolicies));
     }
 
 
