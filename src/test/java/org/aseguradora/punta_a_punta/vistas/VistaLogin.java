328321c63eb396d1dc2fa6920024b95a60491414
Facundo Palleros <facupls10@gmail.com>
2024-07-03 12:09:18 -0300

    agregando test E2E

---
+++ src/test/java/org/aseguradora/punta_a_punta/vistas/VistaLogin.java
@@ -0,0 +1,66 @@
+package org.aseguradora.punta_a_punta.vistas;
+
+import com.microsoft.playwright.Page;
+import org.aseguradora.punta_a_punta.VistaWeb;
+
+public class VistaLogin extends VistaWeb {
+
+
+    public VistaLogin(Page page) {
+        super(page);
+        page.navigate("localhost:8080/spring/login");
+    }
+
+    public String obtenerTituloPrincipalDeLaPagina(){
+        return this.obtenerTextoDelElemento("h2.title");
+    }
+
+    public void escribirEmail(String email){
+        this.escribirEnElElemento("#exampleInputEmail1", email);
+    }
+
+    public void escribirClave(String clave){
+        this.escribirEnElElemento("#exampleInputPassword1", clave);
+    }
+
+    public void darClickEnIniciarSesion(){
+        this.darClickEnElElemento("button.btn.btn-secondary");
+    }
+
+    public void darClickEnBotonMiDeuda(){
+        this.darClickEnElElemento("#buttonMiDeuda");
+    }
+
+    public String obtenerTituloPrincipalDeLaPaginaMiSaldo(){
+        return this.obtenerTextoDelElemento("h2.text-center");
+    }
+
+    public void darClickEnElBotonPagar(Long id){
+        this.darClickEnElElemento("#button_pagar_" + id);
+    }
+
+    public void darClickEnElLinkCotizacion(){
+        this.darClickEnElElemento("#link_cotizacion");
+    }
+    public void darClickEnElBotonCotizarAuto(){
+        this.darClickEnElElemento("#boton_cotizar_auto");
+    }
+
+    public void darClickEnElBotonSiguienteDePasoUno(){
+        this.darClickEnElElemento("#boton_siguiente_paso_uno");
+    }
+
+    public void darClickEnElBotonSiguienteDeGuardarPasoUno(){
+        this.darClickEnElElemento("#boton_siguiente_paso_dos");
+    }
+
+    public void seleccionarTipoDeCobertura(String value){
+        this.seleccionarOption("#tipo_cobertura", value);
+    }
+
+    public void darClickEnElBotonCotizar(){
+        this.darClickEnElElemento("#cotizar_auto");
+    }
+
+
+}
