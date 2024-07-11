2fe2df698d601601eeb49f6a0f74f2a3dec5b346
Silvana Agustina Coscarelli <silvanaagustinacoscarelli@gmail.com>
2024-07-11 01:38:26 -0300

    modificacion de fechas de polizas en bbdd.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,13 +17,13 @@ VALUES ('Lugo Fernando', 'fer120793@gmail.com', '123');
 INSERT INTO policy (insurance_id, customer_id, coverage, start_date, expiration)
 VALUES (1, 1, 200000, '2024-01-01', '2025-01-01');
 INSERT INTO policy (insurance_id, customer_id, coverage, start_date, expiration)
-VALUES (2, 2, 150000, '2023-05-15', '2024-05-15');
+VALUES (2, 2, 150000, '2023-05-15', '2025-05-15');
 INSERT INTO policy (insurance_id, customer_id, coverage, start_date, expiration)
-VALUES (3, 3, 27000, '2022-12-31', '2052-12-31');
+VALUES (3, 3, 27000, '2022-12-31', '2025-12-31');
 INSERT INTO policy (insurance_id, customer_id, coverage, start_date, expiration)
-VALUES (2, 3, 145000, '2022-12-31', '2052-12-31');
+VALUES (2, 3, 145000, '2022-12-31', '2025-12-31');
 INSERT INTO policy (insurance_id, customer_id, coverage, start_date, expiration)
-VALUES (2, 3, 1250000, '2022-12-31', '2023-12-31');
+VALUES (2, 3, 1250000, '2022-12-31', '2024-12-31');
 INSERT INTO policy (insurance_id, customer_id, coverage, start_date, expiration)
 VALUES (2, 3, 1250001, '2023-12-31', '2024-12-31');
 INSERT INTO policy (insurance_id, customer_id, coverage, start_date, expiration)
